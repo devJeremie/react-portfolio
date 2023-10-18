2fb6c545d5ebe5949292ffeff75b74194707b7bb
devJeremie <chabanaisjeremie@gmail.com>
2023-10-18 12:39:44 +0200

    header&css

---
+++ src/playerStats/PlayerStats.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/playerStats.css";
+
+
+const PlayerStats = () => {
+    return(
+        <div id="playerStats">
+            <h1>JEREMIE CHABANAIS LEVEL 41</h1>
+            <div id="playerStats_lines">
+                <span id="player_lines_thick"></span>
+                <span id="player_lines_thin"></span>
+            </div>
+            <h2>Developpeur Fullstack</h2>
+        </div>
+    );
+};
+
+export default PlayerStats;
