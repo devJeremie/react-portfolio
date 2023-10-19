9a92c78e2db54482a8647fbdf32b9925bb4e2983
devJeremie <chabanaisjeremie@gmail.com>
2023-10-19 14:30:19 +0200

    SkillsPageEnd

---
+++ src/skills/skillsData.js
@@ -0,0 +1,60 @@
+const skills = {
+    1: [
+        {
+            title: "HTML",
+            level: 5,
+        },
+        {
+            title: "CSS",
+            level: 4,
+        },
+        {
+            title: "Twig",
+            level: 4,
+        },
+        {
+            title: "Javascript",
+            level: 5,
+        },
+        {
+            title: "ReactJS",
+            level: 3,
+        },
+        {
+            title: "React Native",
+            level: 4,
+        },
+        {
+            title: "Wordpress",
+            level: 3,
+        },
+    ],
+    2: [
+        {
+            title: "Node.js",
+            level: 2,
+        },
+        {
+            title: "MySQL",
+            level: 5,
+        },
+        {
+            title: "PHP",
+            level: 3,
+        },
+        {
+            title: "Symfony",
+            level: 4,
+        },
+        {
+            title: "Git",
+            level: 4,
+        },
+        {
+            title: "Java",
+            level: 2,
+        },
+    ],
+};
+
+export default skills;
