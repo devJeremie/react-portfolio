a3a37a63af0f8f347a4a407d51871f6f4de38732
devJeremie <chabanaisjeremie@gmail.com>
2023-10-19 16:26:59 +0200

    ProjectPageEnd

---
+++ src/projects/projectsData.js
@@ -0,0 +1,37 @@
+import projectOne from "../assets/project-1.png";
+import projectTwo from "../assets/project-2.png";
+import projectThree from "../assets/project-3.png";
+
+const projects = {
+    1: {
+        title: "Some platform",
+        image: projectOne,
+        description: (
+            <>
+                <p>Détailler le projet</p>
+            </>
+        ),
+        github: "https://github.comEtNoteLeLienDuRepo"
+    },
+    2: {
+        title: "Productivity App",
+        image: projectTwo,
+        description: (
+            <>
+                <p>Détailler le projet</p>
+            </>
+        ),
+        github: "https://github.comEtNoteLeLienDuRepo"
+    },
+    3: {
+        title: "glasses sales website",
+        image: projectThree,
+        description: (
+            <>
+                <p>Détailler le projet</p>
+            </>
+        ),
+        github: "https://github.comEtNoteLeLienDuRepo"
+    },
+};
+export default projects;
